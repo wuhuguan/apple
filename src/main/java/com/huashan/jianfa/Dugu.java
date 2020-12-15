c48dd8bf8d451552c0ef2baa9b349759cf8f009b
wuhuguan <731518388@qq.com>
2020-12-15 08:40:59 +0800

    create 01

---
+++ src/main/java/com/huashan/jianfa/Dugu.java
@@ -0,0 +1,11 @@
+package com.huashan.jianfa;
+
+public class Dugu {
+    public static void main(String[] args) {
+
+        System.out.println("独孤九剑好！！！");
+        System.out.println("独孤九剑好！！！");
+        System.out.println("独孤九剑好！！！");
+        System.out.println("独孤九剑好！！！");
+    }
+}
